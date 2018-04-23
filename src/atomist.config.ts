094183969f3a80830b7fa7276da049736539a6cc
Christian Dupuis <cd@atomist.com>
2018-04-23 14:47:45 +0200

    Update atomist.config.ts

--- src/atomist.config.ts
+++ src/atomist.config.ts
@@ -389,7 +389,7 @@ export const configuration: Configuration = {
     ws: {
         compress: false,
         termination: {
-            gracePeriod: 5015,
+            gracePeriod: 5016,
         },
     },
     logging: {
